b728dd7a1960d1c4ca0519b9bef00c9de21f20a1
Bruno Aguiar <aguiarsilva@gmail.com>
2023-12-21 15:06:55 +0100

    updated .h file

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,21 @@
 /*   By: baguiar- <baguiar-@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 14:37:42 by baguiar-          #+#    #+#             */
-/*   Updated: 2023/12/21 14:39:12 by baguiar-         ###   ########.fr       */
+/*   Updated: 2023/12/21 15:06:39 by baguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+#endif
+
+typedef struct s_list 
+{
+  char          *s_buff;
+  struct s_list *next;
+}     t_list;
+
+#endif
