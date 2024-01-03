34fb7f6a86ab9e04cb400c5134f84e46c92e4d7b
Bruno leonardo Aguiar silva <baguiar-@c2s12d60.42wolfsburg.de>
2024-01-03 13:58:41 +0100

    added function to .c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: baguiar- <baguiar-@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 14:38:09 by baguiar-          #+#    #+#             */
-/*   Updated: 2024/01/03 08:53:08 by baguiar-         ###   ########.fr       */
+/*   Updated: 2024/01/03 12:31:09 by baguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,28 @@ static char *fill_buffer(int fd, char *lchar, char *buf)
       break;
   }
   return (lchar);
+}
+
+static char	*put_line(char *line_buf)
+{
+  char    *stack;
+  ssize_t i;
+
+  i = 0;
+  while (line_buf[i] != '\n' && line_buf[i] != '\0')
+  {
+    i++;
+  }
+  if (line_buf[i] == '\0' && line_buf[i] == '\n')
+    return (NULL);
+  line_buf = ft_substr(line_buf, i + 1, ft_strlen(line_buf) - i);
+  if (*line_buf == 0)
+  {
+    free(line_buf);
+    line_buf = NULL;
+  }
+  line_buf[i + 1] = '\0';
+  return (line_buf);
 }
 
 char  *get_next_line(int fd)
