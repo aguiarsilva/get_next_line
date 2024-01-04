341e7b91700ea2d14df969b5e2c9f6c28aecd505
Bruno Aguiar <aguiarsilva@gmail.com>
2024-01-04 14:43:08 +0100

    updated file get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: baguiar- <baguiar-@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 14:38:09 by baguiar-          #+#    #+#             */
-/*   Updated: 2024/01/04 14:38:21 by baguiar-         ###   ########.fr       */
+/*   Updated: 2024/01/04 14:42:59 by baguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,14 +53,14 @@ static char	*put_line(char *line_buf)
   }
   if (line_buf[i] == '\0' && line_buf[i] == '\n')
     return (NULL);
-  line_buf = ft_substr(line_buf, i + 1, ft_strlen(line_buf) - i);
-  if (*line_buf == 0)
+  stack = ft_substr(line_buf, i + 1, ft_strlen(line_buf) - i);
+  if (*stack == 0)
   {
-    free(line_buf);
-    line_buf = NULL;
+    free(stack);
+    stack = NULL;
   }
   line_buf[i + 1] = '\0';
-  return (line_buf);
+  return (stack);
 }
 
 char  *get_next_line(int fd)
