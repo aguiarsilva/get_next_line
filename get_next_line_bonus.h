a890a408ef88d37b312ca81b2eaa2bfb87b6982a
Bruno Aguiar <aguiarsilva@gmail.com>
2024-01-08 14:13:45 +0100

    added bonus files

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -10,4 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 15
+#endif
+# define MAX_FD 10240
+# include <stdlib.h>
+# include <unistd.h>
 
+char  *get_next_line(int fd);
+char  *ft_strchr(const char *s, int c);
+char  *ft_strdup(char *s1);
+size_t  ft_strlen(const char *str);
+char  *ft_substr(char const *s, unsigned int start, size_t len);
+char  *ft_strjoin(char const *s1, char const *s2);
+
+#endif
