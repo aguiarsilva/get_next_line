442acc37f492029e7c3f01ef37e784f07c957936
Bruno leonardo Aguiar silva <baguiar-@c2r14s7.42wolfsburg.de>
2024-03-21 10:33:59 +0100

    corrected files

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -12,82 +12,82 @@
 
 #include "get_next_line_bonus.h"
 
-char *fill_line(int fd, char *lchar, char *buf);
+char	*fill_line(int fd, char *lchar, char *buf);
 char	*put_line(char *line_buf);
 
-char  *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-  static char  *lchar[MAX_FD];
-  char         *buf;
-  char         *line;
+	static char	*lchar[MAX_FD];
+	char		*buf;
+	char		*line;
 
-  buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-  if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-  {
-    free(buf);
-    free(lchar[fd]);
-    buf = NULL;
-    lchar[fd] = NULL;
-    return (NULL);
-  }
-  if (!buf)
-    return (NULL);
-  line = fill_line(fd, lchar[fd], buf);
-  free(buf);
-  buf = NULL;
-  if (!line)
-    return (NULL);
-  lchar[fd] = put_line(line);
-  return (line);
+	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		free(buf);
+		free(lchar[fd]);
+		buf = NULL;
+		lchar[fd] = NULL;
+		return (NULL);
+	}
+	if (!buf)
+		return (NULL);
+	line = fill_line(fd, lchar[fd], buf);
+	free(buf);
+	buf = NULL;
+	if (!line)
+		return (NULL);
+	lchar[fd] = put_line(line);
+	return (line);
 }
 
-char *fill_line(int fd, char *lchar, char *buf)
+char	*fill_line(int fd, char *lchar, char *buf)
 {
-  ssize_t byte;
-  char    *temp;
+	ssize_t	byte;
+	char	*temp;
 
-  byte = 1;
-  while (byte > 0)
-  {
-    byte = read(fd, buf, BUFFER_SIZE);
-    if (byte == -1)
-    {
-      free(lchar);
-      return (NULL);
-    }
-    else if (byte == 0)
-        break;
-    buf[byte] = 0;
-    if (!lchar)
-      lchar = ft_strdup("");
-    temp = lchar;
-    lchar = ft_strjoin(temp, buf);
-    free(temp);
-    temp = NULL;
-    if (ft_strchr(buf, '\n'))
-      break;
-  }
-  return (lchar);
+	byte = 1;
+	while (byte > 0)
+	{
+		byte = read(fd, buf, BUFFER_SIZE);
+		if (byte == -1)
+		{
+			free(lchar);
+			return (NULL);
+		}
+		else if (byte == 0)
+			break ;
+		buf[byte] = 0;
+		if (!lchar)
+			lchar = ft_strdup("");
+		temp = lchar;
+		lchar = ft_strjoin(temp, buf);
+		free(temp);
+		temp = NULL;
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	return (lchar);
 }
 
 char	*put_line(char *line_buf)
 {
-  char    *stack;
-  ssize_t i;
+	char	*stack;
+	ssize_t	i;
 
-  i = 0;
-  while (line_buf[i] != '\n' && line_buf[i] != '\0')
-  {
-    i++;
-  }
-  if (line_buf[i] == 0 || line_buf[1] == 0)
-    return (0);
-  stack = ft_substr(line_buf, i + 1, ft_strlen(line_buf) - i);
-  if (*stack == 0)
-  {
-    free(stack);
-    stack = NULL;
-  }
-  line_buf[i + 1] = 0;
-  return (stack);
+	i = 0;
+	while (line_buf[i] != '\n' && line_buf[i] != '\0')
+	{
+		i++;
+	}
+	if (line_buf[i] == 0 || line_buf[1] == 0)
+		return (0);
+	stack = ft_substr(line_buf, i + 1, ft_strlen(line_buf) - i);
+	if (*stack == 0)
+	{
+		free(stack);
+		stack = NULL;
+	}
+	line_buf[i + 1] = 0;
+	return (stack);
 }
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,108 +6,108 @@
 /*   By: baguiar- <baguiar-@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 14:20:16 by baguiar-          #+#    #+#             */
-/*   Updated: 2024/01/05 14:20:18 by baguiar-         ###   ########.fr       */
+/*   Updated: 2024/01/17 09:58:01 by baguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-char  *ft_strdup(char *s1)
+char	*ft_strdup(char *s1)
 {
-  char    *s2;
-  size_t  i;
+	char	*s2;
+	size_t	i;
 
-  s2 = (char *)malloc(sizeof(char)*(ft_strlen(s1) + 1));
-  if (!s2)
-    return (NULL);
-  i = 0;
-  while (s1[i])
-  {
-    s2[i] = s1[i];
-    i++;
-  }
-  s2[i] = '\0';
-  return (s2);
+	s2 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (!s2)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
 }
 
-size_t  ft_strlen(const char *str)
+size_t	ft_strlen(const char *str)
 {
-  size_t  len;
+	size_t	len;
 
-  len = 0;
-  while (str[len])
-    len++;
-  return (len);
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
 }
 
-char  *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-  size_t  i;
-  char    *s2;
+	size_t	i;
+	char	*s2;
 
-  if (!s)
-    return (NULL);
-  if (start > ft_strlen(s))
-    return (ft_strdup(""));
-  if (len > ft_strlen(s + start))
-    len = ft_strlen(s + start);
-  s2 = (char *)malloc((len + 1) * sizeof(char));
-  if (!s2)
-    return (NULL);
-  i = 0;
-  while (i < len)
-  {
-    s2[i] = s[start];
-    i++;
-    start++;
-  }
-  s2[i] = '\0';
-  return (s2);
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	s2 = (char *)malloc((len + 1) * sizeof(char));
+	if (!s2)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		s2[i] = s[start];
+		i++;
+		start++;
+	}
+	s2[i] = '\0';
+	return (s2);
 }
 
-char  *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-  char  *str;
-  int   lens1;
-  int   lens2;
-  int   j;
-  int   i;
+	char	*str;
+	int		lens1;
+	int		lens2;
+	int		j;
+	int		i;
 
-  lens1 = ft_strlen(s1);
-  lens2 = ft_strlen(s2);
-  i = 0;
-  j = 0;
-  str = (char *)malloc((lens1 + lens2 + 1) * sizeof(char));
-  if (!str)
-    return (NULL);
-  while (s1[i])
-  {
-    str[i] = s1[i];
-    i++;
-  }
-  while (s2[j])
-  {
-    str[i + j] = s2[j];
-    j++;
-  }
-  str[i + j] = '\0';
-  return (str);
+	lens1 = ft_strlen(s1);
+	lens2 = ft_strlen(s2);
+	i = 0;
+	j = 0;
+	str = (char *)malloc((lens1 + lens2 + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
 }
 
-char  *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-  unsigned int  i;
-  char          c1;
+	unsigned int	i;
+	char			c1;
 
-  c1 = (char) c;
-  i = 0;
-  while (s[i])
-  {
-    if (s[i] == c1)
-      return ((char *) &s[i]);
-    i++;
-  }
-  if (s[i] == c1)
-    return ((char *) &s[i]);
-  return (NULL);
+	c1 = (char)c;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c1)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == c1)
+		return ((char *)&s[i]);
+	return (NULL);
 }
